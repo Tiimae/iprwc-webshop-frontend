51aaa27bc7c3f7c970ff5015c528db05ae8cea1b
Tim de Kok <de.kok.ac@gmail.com>
2022-11-21 16:26:22 +0100

    feat: added tailwind and all needed components for nav registration and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchbarComponent } from './navigation/searchbar/searchbar.component';
+import { NavitemsComponent } from './navigation/navitems/navitems.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegistrationComponent,
+    SearchbarComponent,
+    NavitemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navigation/navitems/navitems.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navitems',
+  templateUrl: './navitems.component.html',
+  styleUrls: ['./navitems.component.scss']
+})
+export class NavitemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
