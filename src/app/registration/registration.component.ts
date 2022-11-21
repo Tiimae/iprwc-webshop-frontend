bfab88353895bf9d12d990dc99ea604045e3624b
Tim de Kok <de.kok.ac@gmail.com>
2022-11-21 23:52:44 +0100

    feat: added registration styling

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  firstNameField: string = '';
+  middleNameField: string = '';
+  lastNameField: string = '';
+
+  emailField: string = '';
+  passwordField: string = '';
+  passwordRepeatField: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
