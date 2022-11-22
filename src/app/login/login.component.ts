17375b598bdff940a29c72b2ff62731df382b03d
Tim de Kok <de.kok.ac@gmail.com>
2022-11-22 12:06:44 +0100

    feat: changed login form style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from "../service/auth.service";
 import {ApiConnectorService} from "../service/api-connector.service";
 import {Router} from "@angular/router";
+
+import {faKey, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -10,16 +13,24 @@ import {Router} from "@angular/router";
 })
 export class LoginComponent implements OnInit {
 
-  emailField = 'de.kok.ac@gmail.com';
-  passwordField = 'Test123';
+  // emailField = 'de.kok.ac@gmail.com';
+  // passwordField = 'Test123';
+
+  faKey = faKey;
+  faEnvelope = faEnvelope;
+
+  @ViewChild('f') loginForm: NgForm | undefined;
 
   constructor(private router: Router,) { }
 
   ngOnInit(): void {
   }
 
-  public LogUserIn() {
-    new AuthService().login(this.emailField, this.passwordField).then(r => {
+  public onSubmit() {
+    new AuthService().login(
+      this.loginForm?.form.controls['email'].value,
+      this.loginForm?.form.controls['password'].value
+    ).then(r => {
       if (r.data.payload.jwtToken != null && r.data.payload.userId != null) {
         ApiConnectorService.getInstance().storeJwtToken(r.data.payload.jwtToken);
         ApiConnectorService.getInstance().storeUserId(r.data.payload.userId);
