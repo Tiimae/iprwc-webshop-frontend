1f5179abb1aa988cfa47ce6f1dced4c76be41e2d
Tim de Kok <de.kok.ac@gmail.com>
2022-12-21 12:20:55 +0100

    feat: minor change

--- src/app/_service/data/cartData.service.ts
+++ src/app/_service/data/cartData.service.ts
@@ -25,30 +25,26 @@ export class CartDataService {
     if (items != null) {
       // @ts-ignore
       let item = items.find(item => JSON.parse(item).id === product.id)
-
       if (item != undefined) {
         const newItem = JSON.stringify({
           "id": product.id,
           "amount": JSON.parse(item).amount + amount
         });
-
         // @ts-ignore
         items[items.findIndex(item => JSON.parse(item).id === product.id)] = newItem
         check = true;
+      } else {
+        const newItem = JSON.stringify({
+          "id": product.id,
+          "amount": amount
+        });
+
+        // @ts-ignore
+        items.push(newItem);
+
+        this.products.push(product)
+        this.products$.next(this.products)
       }
-    }
-
-    if (!check) {
-      const newItem = JSON.stringify({
-        "id": product.id,
-        "amount": amount
-      });
-
-      // @ts-ignore
-      items.push(newItem);
-
-      this.products.push(product)
-      this.products$.next(this.products)
     }
 
     localStorage.setItem("cart", JSON.stringify(items))
