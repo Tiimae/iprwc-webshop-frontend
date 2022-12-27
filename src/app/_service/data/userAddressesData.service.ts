ffeb7c43add9fd4a75917f57c913987c5f56a3dc
Tim de kok <de.kok.ac@gmail.com>
2022-12-27 17:07:32 +0100

    feat: added some code to userAddressData service

--- src/app/_service/data/userAddressesData.service.ts
+++ src/app/_service/data/userAddressesData.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import { UserAddressesModel } from "src/app/_models/userAddresses.model";
-import {BehaviorSubject, Subject} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
+import {ApiConnectorService} from "../api-connector.service";
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +9,25 @@ import {BehaviorSubject, Subject} from "rxjs";
 export class UserAddressesDataService {
   userAddresses: UserAddressesModel[] = []
   userAddresses$: Subject<UserAddressesModel[]> = new BehaviorSubject<UserAddressesModel[]>([]);
+
+  constructor(
+    private api: ApiConnectorService
+  ) {
+  }
+
+  getAll(): void {
+
+  }
+
+  getByUserId(userId: string): Observable<UserAddressesModel[]> {
+    const userAddresses: UserAddressesModel[] = []
+
+    this.userAddresses.forEach(address => {
+      if (address.user.id === userId) {
+        userAddresses.push(address);
+      }
+    })
+
+    return of(userAddresses)
+  }
 }
